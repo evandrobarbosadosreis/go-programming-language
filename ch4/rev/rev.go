4c80e07c6232f0b839b9417d49695fb4dec3b5f6
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2023-03-13 19:56:05 -0300

    add rev

---
+++ ch4/rev/rev.go
@@ -0,0 +1,12 @@
+package main
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	a := [...]int{0, 5, 6, 3, 25, 77, 58, 663}
+	reverse(a[:])
+}
